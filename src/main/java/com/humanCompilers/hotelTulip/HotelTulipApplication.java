a5acc30f224d527ba58a5f8c4f9afcbb3fa5f004
Maarten <maartenhandels@gmail.com>
2020-10-20 11:26:48 +0200

    Project setup

---
+++ src/main/java/com/humanCompilers/hotelTulip/HotelTulipApplication.java
@@ -0,0 +1,13 @@
+package com.humanCompilers.hotelTulip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelTulipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelTulipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/humanCompilers/hotelTulip/HotelTulipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humanCompilers.hotelTulip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelTulipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
