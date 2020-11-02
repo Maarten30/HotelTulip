dd7f68dfb0c476eada36a40fa8be675089efb047
Laura <laura.llorente@opendeusto.es>
2020-11-02 23:28:46 +0100

    avances frontend responsive 2

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -12,3 +12,22 @@ setTimeout( () => {
     /*}*/
 
 }, 2000);
+
+/* BOTON ARRIBA -------------------------------*/
+(function ($) {
+    "use strict";
+    $(document).ready(function(){
+        $('.go-up').click(function(){
+            $('body, html').animate({
+                scrollTop: '0px'
+            }, 300);
+        });
+        $(window).scroll(function(){
+            if( $(this).scrollTop() > 0 ){
+                $('.go-up').slideDown(300);
+            } else {
+                $('.go-up').slideUp(300);
+            }
+        });
+    });
+})(jQuery);
